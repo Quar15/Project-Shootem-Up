ddd0d32b156177b0ac68468255142c3e34107958
quar15 <quar5@wp.pl>
2022-06-30 11:37:35 +0200

    add: top speed limit

--- Assets/Scripts/PowerUps/SpeedUp.cs
+++ Assets/Scripts/PowerUps/SpeedUp.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SpeedUp : PowerUp
 {
     public float speedMultiplier = 1.33f;
+    public float speedMax = 25f;
 
     override public void ApplyPowerUp(Player player)
     {
-        player.normalSpeed *= speedMultiplier;
+        player.normalSpeed = Mathf.Clamp(player.normalSpeed * speedMultiplier, 0f, speedMax);
 
         if (player.currentSpeed == player.preciseSpeed)
         {
